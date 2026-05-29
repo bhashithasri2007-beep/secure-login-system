f8a7e52f9275cb9458b4fa3ec49a61a761ea0934
bhashithasri2007-beep <bhashithasri2007@gmail.com>
2026-05-29 10:49:33 +0530

    deployment setup

--- server.js
+++ server.js
@@ -127,8 +127,8 @@ app.get("/logout", (req, res) => {
     });
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(
-        `Server running on http://localhost:${process.env.PORT}`
-    );
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
